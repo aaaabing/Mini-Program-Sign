0965114b0d43aacd85ea382c38f199f29fca50e7
liuzengrui <739522037@qq.com>
2021-08-13 21:36:36 +0800

    feat:新增了采集打卡功能
    Signed-off-by: liuzengrui <739522037@qq.com>

--- src/main/java/com/lzr/controller/Manager.java
+++ src/main/java/com/lzr/controller/Manager.java
@@ -1,17 +1,19 @@
 package com.lzr.controller;
 
-import com.alibaba.fastjson.JSONObject;
+
 import com.lzr.Entity.Point;
 import com.lzr.Entity.Task;
+import com.lzr.Entity.User;
 import com.lzr.mapper.PointMapper;
 import com.lzr.mapper.TaskMapper;
 import com.lzr.mapper.UserMapper;
+import com.lzr.util.UuidUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.stereotype.Repository;
 import org.springframework.web.bind.annotation.*;
 
 import static com.lzr.constant.StatusConstant.INVALID_USER_LOGIN;
+import static com.lzr.constant.StatusConstant.NO_PERMISSION;
 
 /**
  * @author lzr
@@ -74,12 +76,24 @@ public class Manager {
             //获取采集人的信息
             String openid= (String) redisTemplate.opsForValue().get(id);
             //如果openid不存在说明当前用户未登录
-            if (openid.isEmpty()){
+            if (openid==null){
                 return INVALID_USER_LOGIN;
             }
-            // @Todo 权限判断
-            userMapper.getUser(openid).
-            pointMapper.addPoint(point);
+            // 如果不是管理员，返回错误采集
+            if(userMapper.getAdmin(openid)==null){
+                return NO_PERMISSION;
+            }
+            else{
+                //生成ID
+                point.setId(UuidUtils.getFastUuid());
+                //根据当前登录用户查找用户名和id
+                User user=userMapper.getUser(openid);
+                point.setCollectorName(user.getUserName());
+                point.setCollectorId(user.getId());
+                //插入到数据库
+                pointMapper.addPoint(point);
+            }
+
         } catch (Exception e) {
             //@Todo 抛异常
             e.printStackTrace();
